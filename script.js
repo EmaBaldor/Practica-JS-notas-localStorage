39751557fda65bb526e2badd4f96c0c10cde54d1
EmaBaldor <emabaldor@gmail.com>
2020-10-21 19:35:10 -0300

    version 1

---
+++ script.js
@@ -0,0 +1,39 @@
+
+ window.onload = iniciar;
+ 
+ function iniciar(){
+    var btnAgregar = document.getElementById("BtnAgregar");
+    btnAgregar.addEventListener("click", clickBtnAgregar)
+}
+
+function clickBtnAgregar() {
+//traemos txtNotas
+    var txtNota = document.getElementById("txtNota");
+
+    var notas = [];
+    if (localStorage.notas){
+        notas = JSON.parse(localStorage.notas);
+    }  
+    notas.push(txtNota.value);
+
+//queda asignado a localstorage
+//con JSON.stringify transformo arreglo a strig
+    localStorage.notas = JSON.stringify(notas);
+    mostrarNotas();
+    
+}
+
+function mostrarNotas(){
+//tre el div notas
+    var divNotas = document.getElementById("divNotas");
+    //le asigna notas desde localstorage
+    var notas = [];
+    if (localStorage.notas){
+        notas = JSON.parse(localStorage.notas);
+    } 
+    var html = "";
+    for (var nota of notas){
+        html += nota + "<br/>";
+    }  
+    divNotas.innerHTML = html;
+}
